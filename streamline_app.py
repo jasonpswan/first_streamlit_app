0ea12a347da47a7d3cdb2850c58fbb5e6808c0d2
Jason P Swan <58220209+jasonpswan@users.noreply.github.com>
2022-04-19 21:12:44 +0100

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -19,7 +19,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Add a pick list for them to choose the fruit for their smoothie
-fruits_selected = ("Pick some fruit:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+fruits_selected = streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
