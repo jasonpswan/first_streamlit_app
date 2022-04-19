95900050f4e8d5468ece850001dc072a286d92e1
Jason P Swan <58220209+jasonpswan@users.noreply.github.com>
2022-04-19 21:03:21 +0100

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -19,8 +19,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-# Add a pick list so they can choose what they want in their smoothie
-streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-
 # Display the data set so they can see which fruit (and portion sizes) we have available
 streamlit.dataframe(list(my_fruit_list)
