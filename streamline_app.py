910b680209f004f6661ad7c0116a8098c8a8672f
Jason P Swan <58220209+jasonpswan@users.noreply.github.com>
2022-04-19 21:08:44 +0100

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -19,7 +19,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Add a pick list for them to choose the fruit for their smoothie
-fruits_selected = "Pick some fruit:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+fruits_selected = ("Pick some fruit:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
 fruits_to_show = my_fruit_list.loc(fruits_selected)
 
 # Display the data set so they can see which fruit (and portion sizes) we have available
