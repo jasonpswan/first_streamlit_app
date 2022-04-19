5c5c06599c34cf7fed2cb287e1f3ac3d465dcc46
Jason P Swan <58220209+jasonpswan@users.noreply.github.com>
2022-04-19 20:48:34 +0100

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title ('My Mum\'s New Healthy Diner')
 
-streamlit.header ('Breakfast Menu')
+streamlit.header ('Breakfast Favourites')
 streamlit.text ('🥣 Omega 3 and Bluberry Oatmeal')
 streamlit.text ('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
